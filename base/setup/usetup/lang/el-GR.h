38544607f0b1e6960b03f3bc7a5b15cd570d747f
ErrorTek <jakub.baranski.33@gmail.com>
2023-12-25 14:04:07 +0000

    Windivs7 Stable Start Menu banner, Setup fixes

--- base/setup/usetup/lang/el-GR.h
+++ base/setup/usetup/lang/el-GR.h
@@ -8,7 +8,7 @@ static MUI_ENTRY elGRChangelogPageEntries[] =
         {
             4,
             3,
-            " Windivs7 RC (build 7600) Setup ",
+            " Windivs7 RTM (build 7600) Setup ",
             TEXT_STYLE_UNDERLINE,
             TEXT_ID_STATIC
         },
@@ -1165,7 +1165,7 @@ static MUI_ENTRY elGRChangeSystemPartition[] =
     {
         4,
         3,
-        " Windivs7 RC (build 7600) Setup ",
+        " Windivs7 RTM (build 7600) Setup ",
         TEXT_STYLE_UNDERLINE,
         TEXT_ID_STATIC
     },
@@ -1627,7 +1627,7 @@ static MUI_ENTRY elGRBootLoaderInstallPageEntries[] =
     {
         4,
         3,
-        " Windivs7 RC (build 7600) Setup ",
+        " Windivs7 RTM (build 7600) Setup ",
         TEXT_STYLE_UNDERLINE,
         TEXT_ID_STATIC
     },
