60cf0db1fd6b68260f2c87009b7425d2b0be83b3
jpb <jakub.baranski.33@gmail.com>
2024-03-17 11:38:03 +0000

    Update version

--- base/setup/usetup/lang/el-GR.h
+++ base/setup/usetup/lang/el-GR.h
@@ -8,7 +8,7 @@ static MUI_ENTRY elGRChangelogPageEntries[] =
         {
             4,
             3,
-            "Windivs7 build 7899 Setup",
+            "Windivs7 build 7927 Setup",
             TEXT_STYLE_UNDERLINE,
             TEXT_ID_STATIC
         },
@@ -22,7 +22,7 @@ static MUI_ENTRY elGRChangelogPageEntries[] =
         {
             8,
             8,
-            "Windivs7+ RTM, build 7899.1",
+            "Windivs7.5 RTM, build 7927.1",
             TEXT_STYLE_NORMAL,
             TEXT_ID_STATIC
         },
@@ -1165,7 +1165,7 @@ static MUI_ENTRY elGRChangeSystemPartition[] =
     {
         4,
         3,
-        "Windivs7 build 7899 Setup",
+        "Windivs7 build 7927 Setup",
         TEXT_STYLE_UNDERLINE,
         TEXT_ID_STATIC
     },
@@ -1627,7 +1627,7 @@ static MUI_ENTRY elGRBootLoaderInstallPageEntries[] =
     {
         4,
         3,
-        "Windivs7 build 7899 Setup",
+        "Windivs7 build 7927 Setup",
         TEXT_STYLE_UNDERLINE,
         TEXT_ID_STATIC
     },
